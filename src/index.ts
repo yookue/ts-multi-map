87af326702c0f5df41a5fa92384692d81622497d
yookue <yookue@outlook.com>
2024-09-01 21:17:47 +0800

    Update index

--- src/index.ts
+++ src/index.ts
@@ -15,10 +15,10 @@
  */
 
 
-export * from './type/declaration';
 export {MultiKeyMap} from './util/MultiKeyMap';
 export {MultiValueMap} from './util/MultiValueMap';
 export {RangeMap} from './util/RangeMap';
 export {ReadonlyMultiKeyMap} from './util/ReadonlyMultiKeyMap';
 export {ReadonlyMultiValueMap} from './util/ReadonlyMultiValueMap';
 export {ReadonlyRangeMap} from './util/ReadonlyRangeMap';
+export * from './type/declaration';
